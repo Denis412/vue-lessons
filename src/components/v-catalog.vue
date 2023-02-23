97c6c0f6887f7caf3ae179a90209f1b47bde16d7
Denis412 <d-a-malyshev@yandex,ru>
2023-02-23 19:08:03 +0300

    vuex

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -14,85 +14,16 @@
 
 <script>
 import VCatalogItem from "@/components/v-catalog-item.vue";
-import { mapActions, mapGetters, mapMutations } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "v-catalog",
   components: { VCatalogItem },
-  computed: mapGetters(["allProducts"]),
-  // data() {
-  //   return {
-  //     products: [
-  //       {
-  //         image: "1.png",
-  //         name: "Analyst",
-  //         price: 2100,
-  //         article: "T1",
-  //         available: true,
-  //         category: "it products",
-  //         quantity: 0,
-  //         about: "Hi i'm an Analyst! Nice to meet you!",
-  //       },
-  //       {
-  //         image: "2.png",
-  //         name: "Designer",
-  //         price: 3150,
-  //         article: "T2",
-  //         available: true,
-  //         category: "it products",
-  //         quantity: 0,
-  //         about: "Hey my friend! Nice to meet you!",
-  //       },
-  //       {
-  //         image: "3.png",
-  //         name: "Marketer",
-  //         price: 4200,
-  //         article: "T3",
-  //         available: false,
-  //         category: "it products",
-  //         quantity: 0,
-  //         about: "Hello! How have you been?",
-  //       },
-  //       {
-  //         image: "4.png",
-  //         name: "Developer",
-  //         price: 5300,
-  //         article: "T4",
-  //         available: true,
-  //         category: "it products",
-  //         quantity: 0,
-  //         about: "Nice to meet you! What about Vue?",
-  //       },
-  //       {
-  //         image: "5.png",
-  //         name: "Business Accelerator",
-  //         price: 6500,
-  //         article: "T5",
-  //         available: false,
-  //         category: "it products",
-  //         quantity: 0,
-  //         about: "Hey! We'll solve any problems!",
-  //       },
-  //       {
-  //         image: "6.png",
-  //         name: "Engineer",
-  //         price: 8700,
-  //         article: "T6",
-  //         available: true,
-  //         category: "it products",
-  //         quantity: 0,
-  //         about: "Hi! I've many solutions!",
-  //       },
-  //     ],
-  //   };
-  // },
   mounted() {
     this.fetchProducts();
   },
-  methods: {
-    ...mapActions(["fetchProducts"]),
-    ...mapMutations(["addProductInCart"]),
-  },
+  computed: mapGetters(["allProducts"]),
+  methods: mapActions(["fetchProducts"]),
 };
 </script>
 
